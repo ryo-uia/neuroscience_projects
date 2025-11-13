d925ef17d1029beb5258e8e220dabf264f3090a3
ryo-uia <ryo.iwai@uia.no>
2025-11-13 21:36:18 +0100

    Update

--- Practice/learning.py
+++
@@ -1,7 +0,0 @@
-#create a function which takes two numbers as an argument
-#this function should add these two numbers together 
-# and return the result
-
-def two_numbers(a, b):
-    c = a + b
-    return c
--- Practice/learning_imports.py
+++
@@ -1,4 +0,0 @@
-from learning import two_numbers
-
-new=two_numbers(a=5, b=10)
-print(new)
